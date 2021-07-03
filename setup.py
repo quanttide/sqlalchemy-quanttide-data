a90055c3c47573e9570a521a47afc67ecfce8624
huangrihang <huangrihang@quanttide.com>
2021-07-03 14:25:09 +0800

    feat(*): 支持安装时按需一并安装各模块依赖

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from setuptools import setup
 
-setup(name='sql-utils', version='0.3.0', packages=['sql_utils'], url='', license='Apache 2.0', author='QuantTide Inc.',
-    author_email='tech@quanttide.com', description='')
+setup(name='sql-utils', version='0.4.0', packages=['sql_utils'], url='', license='Apache 2.0', author='QuantTide Inc.',
+      author_email='tech@quanttide.com', description='',
+      extras_require={'sqlalchemy': ['sqlalchemy', 'tablib'], 'records': ['records'], 'mysqlclient': ['mysqlclient'],
+                      'pymysql': ['pymysql'], 'mysql': ['mysqlclient'], 'postgresql': ['psycopg2'],
+                      'pgsql': ['psycopg2'], 'psycopg2': ['psycopg2'], 'psycopg2-binary': ['psycopg2-binary'],
+                      'sqlserver': ['pymssql'], 'oracle': ['cx_Oracle']})
